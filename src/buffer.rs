6af957d965200f6e4dd741554fdf86160c615ee7
pmk21 <prithvikrishna49@gmail.com>
2020-04-16 21:27:03 +0530

    Added documentation for buffer.rs

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,16 +1,39 @@
+//! # Buffer
+//! 
+//! A small interface to read user input from stdin.
+
 use std::io::{self, BufRead};
 
+/// Structure to hold the user input
 pub struct InputBuffer {
     pub buffer: String,
 }
 
 impl InputBuffer {
+    /// Returns a new InputBuffer which contains an empty buffer
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use crate::buffer::InputBuffer;
+    /// let input_buffer = InputBuffer::new();
+    /// ```
     pub fn new() -> Self {
         InputBuffer {
             buffer: String::new(),
         }
     }
 
+    /// Reads user input from stdin
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use crate::buffer::InputBuffer;
+    /// let input_buffer = InputBuffer::new();
+    /// // Input from stdin present in input_buffer.buffer
+    /// input_buffer.read_input();
+    /// ```
     pub fn read_input(&mut self) {
         self.buffer.clear();
         let stdin = io::stdin();
